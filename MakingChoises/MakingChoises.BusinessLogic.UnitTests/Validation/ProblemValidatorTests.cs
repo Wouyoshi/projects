38d9e4a72be1c572d4d28c0b73a12fd7142ed24c
Wouyoshi <wschutten@gmail.com>
2014-06-02 22:16:14 +0200

    Wasn't added for some reason

---
+++ MakingChoises/MakingChoises.BusinessLogic.UnitTests/Validation/ProblemValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProblemValidatorTests.cs" company="Wouyoshi BV">
+//   W. Schutten
+// </copyright>
+// <summary>
+//   The problem validator tests.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace MakingChoises.BusinessLogic.UnitTests.Validation
+{
+    using MakingChoises.BusinessLogic.Validation;
+    using MakingChoises.Model;
+
+    using Microsoft.Practices.EnterpriseLibrary.Validation;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>The problem validator tests.</summary>
+    [TestClass]
+    public class ProblemValidatorTests
+    {
+        #region Public Methods and Operators
+
+        /// <summary>The problem validator test.</summary>
+        [TestMethod]
+        public void ProblemValidatorTest()
+        {
+            var problemValidator = new ProblemValidator();
+            var problem = new Problem();
+            ValidationResults result = problemValidator.Validate(problem);
+        }
+
+        #endregion
+    }
+}
