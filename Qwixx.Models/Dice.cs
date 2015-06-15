cfc6aef41b6f1076064e1790232e8b54b61982d5
wouyoshi <wschutten@gmail.com>
2015-06-15 07:33:41 +0200

    Revert "Revert "Qwixx commit""
    This reverts commit e0ca61340e969871477540dce8fd8e68fb6b7bcd.

---
+++ Qwixx.Models/Dice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qwixx.Models
+{
+    public class Dice : IEnumerable<Die>
+    {
+        private readonly IList<Die> dice;
+
+        public Dice(IList<Die> dice)
+        {
+            if (dice == null)
+            {
+                throw new ArgumentNullException("dice");
+            }
+            this.dice = dice;
+        }
+
+        public Die this[int i]
+        {
+            get { return dice[i]; }
+            private set { dice[i] = value; }
+        }
+
+        public IEnumerator<Die> GetEnumerator()
+        {
+            return dice.GetEnumerator();
+        }
+
+        public void RemoveDie(Die die)
+        {
+            if (die == null)
+            {
+                throw new ArgumentNullException("die");
+            }
+            dice.Remove(die);
+        }
+
+        public void RollAll()
+        {
+            foreach(var die in dice)
+            {
+                die.Roll();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return dice.GetEnumerator();
+        }
+    }
+}
---
+++ Qwixx.Models/Line.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qwixx.Models
+{
+    public class Line : IEnumerable<LineNumber>
+    {
+        private readonly IList<LineNumber> lineNumbers;
+        public Ordering Ordering { get; private set; }
+
+        public Color Color { get; private set; }
+
+        
+
+        public Line(Color color)
+        {
+            if (color == null)
+            {
+                throw new ArgumentNullException("color");
+            }
+
+            var lineNumbers = new List<LineNumber>() { };
+            IList<string> numbers = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+            var ordering = Ordering.Ascending;
+            if (color == Color.Green || color == Color.Blue)
+            {
+                ordering = Models.Ordering.Descending;
+                numbers = (IList<string>)numbers.Reverse();
+            }
+            numbers.Add("lock");
+            foreach (var number in numbers)
+            {
+                lineNumbers.Add(new LineNumber(number));
+            }
+
+            this.lineNumbers = lineNumbers;
+            this.Ordering = ordering;
+            this.Color = color;
+        }
+        public Line(IList<LineNumber> lineNumbers, Ordering ordering, Color color)
+        {
+            if (lineNumbers == null)
+            {
+                throw new ArgumentNullException("lineNumbers");
+            }
+            if (color == null)
+            {
+                throw new ArgumentNullException("color"); 
+            }
+
+            this.lineNumbers = lineNumbers;
+            this.Ordering = ordering;
+            this.Color = color;
+        }
+
+        public LineNumber this[int i]
+        {
+            get { return lineNumbers[i]; }
+            private set { lineNumbers[i] = value; }
+        }
+
+        public IEnumerator<LineNumber> GetEnumerator()
+        {
+            return lineNumbers.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return lineNumbers.GetEnumerator();
+        }
+    }
+}
