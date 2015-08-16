f1a89e08a2dd9a59982cb76a142877059edcaa93
wouyoshi <wschutten@gmail.com>
2015-08-16 20:55:14 +0200

    Added factory and stuff

--- Qwixx.Website/Scripts/app.js
+++ Qwixx.Website/Scripts/app.js
@@ -3,13 +3,13 @@
     var app = angular.module("qwixx", []);
 
     app.controller("qwixxController", [
-        "$scope", function($scope) {
-            $scope.red = { rowClass: "qwixx-row-1", imageSrc: "Content/Icons/lock-icon-red.png" };
-            $scope.yellow = { rowClass: "qwixx-row-2", imageSrc: "Content/Icons/lock-icon-yellow.png" };
-            $scope.green = { rowClass: "qwixx-row-3", imageSrc: "Content/Icons/lock-icon-green.png" };
-            $scope.blue = { rowClass: "qwixx-row-4", imageSrc: "Content/Icons/lock-icon-blue.png" };
+        "$scope", "scoreService", function ($scope, scoreService) {
+            $scope.red = { color: "red", rowClass: "qwixx-row-1", imageSrc: "Content/Icons/lock-icon-red.png", order: "asc" };
+            $scope.yellow = { color: "yellow", rowClass: "qwixx-row-2", imageSrc: "Content/Icons/lock-icon-yellow.png", order: "asc" };
+            $scope.green = { color: "green", rowClass: "qwixx-row-3", imageSrc: "Content/Icons/lock-icon-green.png", order: "desc" };
+            $scope.blue = { color: "blue", rowClass: "qwixx-row-4", imageSrc: "Content/Icons/lock-icon-blue.png", order:"desc" };
             $scope.rules = { rowClass: "qwixx-row-5" };
-            $scope.score = { rowClass: "qwixx-row-6" };
+            $scope.score = { rowClass: "qwixx-row-6", scoreService: scoreService };
         }
     ]);
     app.directive("qwixxNumberRow", function() {
@@ -74,11 +74,18 @@
         };
     });
     app.controller("qwixxNumberController", [
-        "$scope", function ($scope) {
+        "$scope", "scoreService", function ($scope, scoreService) {
             $scope.qwixxSelected = false;
             $scope.numberClicked = function () {
-                this.qwixxSelected = true;
+                $scope.qwixxSelected = true;
+                var color = $scope.$parent.$parent.$parent.row.color;
+                if (!$scope.$parent.number) {
+                    scoreService.addLock(color);
+                    return;
+                }
 
+                var number = $scope.$parent.number.numberText;
+                scoreService.addNumber(color, number);
             }
         }
     ]);
@@ -112,5 +119,99 @@
             restrict: "E",
             templateUrl: "qwixx-wasted-dice-row.html"
         };
+    });
+    app.directive("qwixxWasted", function () {
+        return {
+            restrict: "E",
+            templateUrl: "qwixx-wasted.html"
+        };
+    });
+    app.factory("scoreService", function() {
+        var scores = {};
+        scores.red = {};
+        scores.red.list = [];
+        scores.red.total = 0;
+        scores.yellow = {};
+        scores.yellow.list = [];
+        scores.yellow.total = 0;
+        scores.green = {};
+        scores.green.list = [];
+        scores.green.total = 0;
+        scores.blue = {};
+        scores.blue.list = [];
+        scores.blue.total = 0;
+        scores.wasted = {};
+        scores.wasted.list = [];
+        scores.wasted.total = 0;
+
+        scores.getList = function(color) {
+            if (!color) {
+                return null;
+            }
+            switch(color) {
+                case "red":
+                    return scores.red.list;
+                case "yellow":
+                    return scores.yellow.list;
+                case "green":
+                    return scores.green.list;
+                case "blue":
+                    return scores.blue.list;
+                default:
+                    return null;
+            }
+        }
+        scores.addLock = function(color) {
+            if (!color) {
+                return;
+            }
+            var list = scores.getList(color);
+            if (list == null) {
+                return;
+            }
+            if (list.indexOf("lock") >= 0) {
+                return;
+            }
+            list.push("lock");
+            scores.recalculate();
+        }
+        scores.addNumber = function (color, number) {
+            // validate.
+            if (!color || !number) {
+                return;
+            }
+            if (isNaN(number)) {
+                return;
+            }
+            // make sure it's an int value.
+            var intValue = Math.floor(number);
+            if (intValue < 2 || number > 12) {
+                return;
+            }
+            var list = scores.getList(color);
+            if (list == null) {
+                return;
+            }
+            if (list.indexOf(intValue) >= 0) {
+                return;
+            }
+            // Add the score to the list.
+            list.push(intValue);
+            scores.recalculate();
+        }
+        scores.recalculate = function() {
+            var calc = function (length) {
+                var total = 0;
+                for (var i = 0; i <= length; i++) {
+                    total += i;
+                }
+                return total;
+            }
+            scores.red.total = calc(scores.getList("red").length);
+            scores.yellow.total = calc(scores.getList("yellow").length);
+            scores.green.total = calc(scores.getList("green").length);
+            scores.blue.total = calc(scores.getList("blue").length);
+        }
+        return scores;
     });
 })();
