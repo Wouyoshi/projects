da477df7700b718e23e273ed19c92e17e6708434
wouyoshi <wschutten@gmail.com>
2015-09-25 09:17:19 +0200

    Commiting new tile

---
+++ Qwixx.Website/Scripts/gameTile.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    "use strict";
+    var app = angular.module("qwixx");
+
+    app.directive("gameTile", function () {
+        return {
+            restrict: "E",
+            templateUrl: "game-tile.html",
+            scope: {
+                playerCount: "=",
+                maxPlayerCount: "=",
+                gameName: "=",
+                hostName: "="
+            }
+        };
+    });
+})();
