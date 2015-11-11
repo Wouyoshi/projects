686a4ee68214e98ef44871bfebc32fc9de6bda82
wouyoshi <wschutten@gmail.com>
2015-11-11 09:14:30 +0100

    updated factory

--- Qwixx.Website/Scripts/gameOverview.js
+++ Qwixx.Website/Scripts/gameOverview.js
@@ -2,26 +2,10 @@
     "use strict";
     var app = angular.module("qwixx");
 
-    app.factory("gameList", function() {
 
-        var gameList = {};
-        gameList.getGames = function () {
-            var games = [];
-            $http({
-                method: 'GET',
-                url: 'http://localhost:/Qwixx.WebAPI/GameIntention'
-            }).then(function successCallback(response) {
-                games = response;
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
-            return games;
-        };
-    });
     
     app.controller("gameOverviewController", [
-        "$scope", function ($scope) {
+        "$scope", "gameListService", function ($scope, gameListService) {
             $scope.player = {
                 playerName: ""
             };
@@ -30,13 +14,15 @@
                 maxPlayerCount: 5
             };
 
-            $scope.tiles =
-                [{ playerCount: 3, maxPlayerCount: 5, hostName: "Host name", gameName: "Game name", tileClass: "tile-1", dieColor: "red", players: [] },
+            $scope.gameList = gameListService;
+
+            $scope.tiles = gameListService.getGames();
+               /* [{ playerCount: 3, maxPlayerCount: 5, hostName: "Host name", gameName: "Game name", tileClass: "tile-1", dieColor: "red", players: [] },
                  { playerCount: 1, maxPlayerCount: 5, hostName: "Cool host", gameName: "Awesome game", tileClass: "tile-2", dieColor: "yellow", players: [] },
                  { playerCount: 3, maxPlayerCount: 5, hostName: "Pipo de Clown", gameName: "Super game", tileClass: "tile-4", dieColor: "blue", players: [] },
                  { playerCount: 3, maxPlayerCount: 5, hostName: "Hostie", gameName: "Tostie", tileClass: "tile-3", dieColor: "green", players: [] },
                  { playerCount: 3, maxPlayerCount: 5, hostName: "Hostie de Hostname", gameName: "Gostie de game name", tileClass: "tile-3", dieColor: "green", players: [] }];
-
+*/
             $scope.addGame = function () {
                 $scope.tiles.push(
                 {
@@ -65,5 +51,33 @@
             templateUrl: "game-overview.html",
             controller: "gameOverviewController"
         };
+    });
+    app.factory("gameListService", function () {
+
+        var gameList = {};
+        gameList.getGames = function () {
+            var games = [];
+            $http({
+                method: 'GET',
+                url: 'http://localhost/Qwixx.WebAPI/GameIntention'
+            }).then(function successCallback(response) {
+                games = response;
+            }, function errorCallback(response) {
+                var getResp = response;
+                getResp = 2;
+            });
+            return games;
+        };
+        gameList.addGame = function (game) {
+            $http.post('http://localhost/Qwixx.WebAPI/GameIntention', game)
+                .then(function successCallback(response) {
+                    response = response;
+                }, function errorCallback(response) {
+                        // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    response = response;
+                    });
+        };
+        return gameList;
     });
 })();
